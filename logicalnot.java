dacee15891bd56cfa934357aae530064906a6afe
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:27:00 +0530

    Add files via upload

---
+++ logicalnot.java
@@ -0,0 +1,10 @@
+public class logicalnot {
+    public static void main(String[] args) {
+        boolean isRaining =false;
+        if(isRaining){
+            System.out.println("you can go outside without an umbrella.");
+        }else{
+                System.out.println("take an umbrella");
+            }
+        }
+    }
