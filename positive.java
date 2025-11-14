6c7712662f4bcefcb85e1909e07d23fd5b84c73f
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:29:07 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,9 @@
+public class positive {
+    public static void main(String[] args){
+        int num = 0;
+        String result = (num>0)? "positive" :(num<0)? "negative" : "zero";
+        System.out.println("the numbere " +num+ " is " +result);
+
+    }
+
+}
