842969c79a3bcce95d35412a348d2b0a4e016ead
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:24:13 +0530

    Add files via upload

---
+++ grade.java
@@ -0,0 +1,11 @@
+public class grade {
+    public static void main(String[] args){
+        int marks = 82;
+        String grade = (marks>=90)?"A":
+                      (marks>=75)?"B":
+                      (marks>=50)?"C":"F";
+        System.out.println("Grade:" +grade);
+
+    }
+
+}
