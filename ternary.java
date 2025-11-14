d66d5eef0f9f25d69b6875c0763d67a8e3d885a2
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:30:23 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,8 @@
+public class ternary {
+    public static void main(String[] aregs) {
+        int number = 11;
+        String result=(number%2==0)?"Even":"Odd";
+    System.out.println("the number " + number +" is " +result);
+    }
+
+}
