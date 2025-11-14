ad2581d68469dc427822b792b70ec0cfc0a6977b
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:28:00 +0530

    Add files via upload

---
+++ mixedoperators.java
@@ -0,0 +1,8 @@
+public class mixedoperators {
+    public static void main(String[] args){
+        int a=4,b=2,c=7;
+        int result = ++a*(b--+~c)-(-b);
+        System.out.println("result = " +result);
+    }
+
+}
