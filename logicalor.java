d57428d6f3bbf5e1d7fab0bd7d29a85c1ba70aa1
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:27:39 +0530

    Add files via upload

---
+++ logicalor.java
@@ -0,0 +1,14 @@
+public class logicalor {
+    public static void main(String[] args) {
+        boolean isStudent=true;
+        boolean isTeacher=false;
+        if(isStudent || isTeacher){
+            System.out.println("you can enter the campus");
+
+        } else{
+            System.out.println("access denied.");
+            
+        }
+    }
+
+}
