a360521f0c4214a776d37fd26e00db2695126efa
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:24:35 +0530

    Add files via upload

---
+++ Greater.java
@@ -0,0 +1,9 @@
+public class Greater {
+    public static void main(String[] args) {
+        int x=15 , y=10;
+        int a=5 , b=10;
+        System.out.println("x>y : " +(x>y));
+        System.out.println("a<b : " +(a<b));
+    }
+
+}
