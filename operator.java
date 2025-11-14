1e17325d11e2fcb082f3611340323402e50b41ce
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:28:35 +0530

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,7 @@
+public class operator {
+    public static void main(String[] args){
+        int result = 10 +5 * 2;
+        System.out.println("result without parenthes is " +result );
+    }
+
+}
