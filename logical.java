8906e07dd150f63cc91cfb7a48cad7f2f6f34bc5
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:26:40 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,11 @@
+public class logical {
+    public static void main(String[] args){
+    int age =25;
+    boolean haslicense = true;
+    if(age>18 && haslicense){
+        System.out.println("elligible to drive.");
+    }else {
+        System.out.println("not elligible tp drive.");
+    }
+}
+}
