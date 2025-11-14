fa489297eaa088db3de64ad37befe324f35487ca
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:28:51 +0530

    Add files via upload

---
+++ parentheses.java
@@ -0,0 +1,7 @@
+public class parentheses {
+    public static void main(String[] args){
+        int result = (10+5)*2;
+        System.out.println("result with parentheses = " +result);
+    }
+
+}
