f396f1f93217d6222ac12dccdb853e4dd6a34c32
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-14 18:25:09 +0530

    Add files via upload

---
+++ leftshift.java
@@ -0,0 +1,13 @@
+public class leftshift {
+    public static void main(String[] args) {
+        int a=5;
+        int b=20;
+        int c= -20;
+        int result = a<<2;
+        int result1=b>>2;
+        int result2=c>>>2;
+        System.out.println("a<<2 = "+result);
+        System.out.println("b>>2="+result1);
+        System.out.println("c>>>2 =" +result2);
+    }
+}
