9457a325f499918fdb29092c31dd117390958bf2
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-16 22:22:27 +0530

    Add files via upload

---
+++ adding.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Adding
+{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter two numbers");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= a+b;
+        System.out.println("addition is "+c);
+    }
+}
