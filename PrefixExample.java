e28643bf5642306ac944d030a087e814d7c71714
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-16 22:30:09 +0530

    Add files via upload

---
+++ PrefixExample.java
@@ -0,0 +1,8 @@
+public class PrefixExample{
+    public static void main(String[]args){
+        int a=5;
+        int b =3;
+        int result =++a*b+2;
+        System.out.print("result :"+result);
+       }
+}
