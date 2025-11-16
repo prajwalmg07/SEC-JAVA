f11d4b4469e7e65ca40998fa69383c674e3f59a1
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-16 22:23:32 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,17 @@
+
+public class AssignmentOperatorsDemo {
+    public static void main(String[] args) {
+
+        int num = 10;   // initial value
+        System.out.println("Initial value: " + num);
+
+        num += 5;   // num = num + 5
+        System.out.println("After += 5: " + num);
+
+        num -= 3;   // num = num - 3
+        System.out.println("After -= 3: " + num);
+
+        num *= 4;   // num = num * 4
+        System.out.println("After *= 4: " + num);
+    }
+}
