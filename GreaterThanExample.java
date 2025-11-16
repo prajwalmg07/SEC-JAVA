013c613a8b3e836379de1881cbf8967a5e404469
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-16 22:26:22 +0530

    Add files via upload

---
+++ GreaterThanExample.java
@@ -0,0 +1,10 @@
+public class GreaterThanExample{
+    public static void main(String[]args){
+        int x=15;
+        int y=10;
+        int a=5;
+        int b=12;
+        System.out.println("a<b :"+(a<b));
+        System.out.println("x>y :"+(x>y));
+    }
+}
