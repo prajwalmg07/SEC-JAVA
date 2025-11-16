4483832532a39137a3bdeece9c5cca38a0e795e6
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-16 22:26:52 +0530

    Add files via upload

---
+++ LetShiftExample.java
@@ -0,0 +1,10 @@
+public class LetShiftExample{
+    public static void main(String[]args){
+        int a =5;
+        int b =20;
+        int result =a<<2;
+        int result1 =b>>2;
+        System.out.println("a<<2 ="+result);
+        System.out.println("b>>2 ="+result1);
+    }
+}
