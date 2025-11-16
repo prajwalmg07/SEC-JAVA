622402409eceb3c889199844b2581d03c385872d
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-16 22:42:27 +0530

    Add files via upload

---
+++ Modulusex.java
@@ -0,0 +1,8 @@
+public class Modulusex{
+    public static void main(String[]args){
+        int a =25;
+        int b = 4;
+        int remainder = a%b;
+        System.out.println("Remainder :"+remainder);
+    }
+}
