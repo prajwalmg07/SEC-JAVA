cee70ac565e363fadd892d3c6b55e705a2ede4c3
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-16 22:26:37 +0530

    Add files via upload

---
+++ IncDecExample.java
@@ -0,0 +1,10 @@
+public class IncDecExample{
+    public static void main(String[]args){
+        int x=10;
+        System.out.println("Original :"+x);
+        System.out.println("Post-increment :"+(x++));
+        System.out.println("After podt-increment :"+x);
+        System.out.println("pre-increment :"+(++x));
+        System.out.println("pre-decrement :"+(--x));
+    }
+}
