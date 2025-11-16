5dd253ead1076aacfd1a1a6a72f79d58d014d41a
Prajwal M Gowda <prajwalmgowda1413@gmail.com>
2025-11-16 22:29:10 +0530

    Add files via upload

---
+++ Unsignedrightsgiftexample.java
@@ -0,0 +1,10 @@
+public class Unsignedrightsgiftexample{
+    public static void main(String[]args){
+        int a= -20;
+        int x =10;
+        int y=10;
+        int result =a>>>2;
+        System.out.println("x==y :"+(x==y));
+        System.out.println("a>>>2 ="+result);
+    }
+}
